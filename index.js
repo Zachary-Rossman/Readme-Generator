9917a9cf3be64327aee8062e36d1373ba329a862
Zachary-Rossman <zrossman@yahoo.com>
2023-01-06 12:09:17 -0600

    app now generates README.md file with node

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         message: 'What is your github username?',
-        name: 'github',
+        name: 'title',
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(fileName, JSON.stringify(data));
+    const exportData = generateMarkdown(data);
+    return fs.writeFileSync(fileName, exportData);
 }
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.createPromptModule(questions).then((response) => {
+function init () {
+    inquirer.prompt(questions).then((response) => {
         console.log(response);
         writeToFile("README.md", response);
-    })
+    });
 }
 
 // Function call to initialize app
